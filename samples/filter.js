7bc8a7f7abb7a4f9419ad71726da5a50485deaf4
Jonatan Vicente <it@jonatanvicente.com>
2021-02-16 12:44:28 +0100

    Fixing

--- samples/filter.js
+++ samples/filter.js
@@ -1,25 +1,5 @@
 
 
-//example 1
-function asyncThing (value) {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(value), 100);
-    });
-}
-
-async function main () {
-    return [1,2,3,4].filter(async (value) => {
-        const v = await asyncThing(value);
-        return v * 2;
-    });
-}
-
-main()
-    .then(v => console.log(v))
-    .catch(err => console.error(err));
-
-
-//example 2
 let checkAge = (age) => age >= 18;
 
 // Autoinv: filter over ages
